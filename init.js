25c502b8e30dc28181778214a04d791363dd0eef
Juan Cuadra <jcb8793@gmail.com>
2017-11-02 20:55:00 +0100

    Disable doble touch

--- init.js
+++ init.js
@@ -2,6 +2,12 @@
 
 function init() {
   var parentElement = document.getElementsByTagName("body");
+  $(document).on("touchmove", function(event) {
+    if (event.originalEvent.scale !== 1) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  });
   var game = new Game(parentElement);
   game.createSplash();
   console.log("hey");
